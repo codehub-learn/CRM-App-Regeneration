d15fd8e3ecdaed4bf23a14c944e8792fb71def41
dimitris <iracleous@gmail.com>
2021-05-12 21:13:32 +0300

    Crud

--- CRM-App/Program.cs
+++ CRM-App/Program.cs
@@ -1,5 +1,9 @@
-﻿using CRM_App.Model;
+﻿using CRM_App.Database;
+using CRM_App.Model;
+using CRM_App.Service;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 
 namespace CRM_App
 {
@@ -7,17 +11,48 @@ namespace CRM_App
     {
         static void Main(string[] args)
         {
+            using CrmDbContext db = new CrmDbContext();
+            CustomerService customerService = new(db);
+            //Customer customer = new()
+            //{
+            //    FirstName = "Christos",
+            //    LastName = "Christou",
+            //    Address = "Athens"
+            //};
+
+            //Console.WriteLine($"customerId = {customer.CustomerId}"
+            //    + $" FirstName = {customer.FirstName}");
+
+            //
+            //customer = customerService.CreateCustomer(customer);
+            //if (customer != null)
+            //{
+            //    Console.WriteLine($"customerId = {customer.CustomerId}"
+            //    + $" FirstName = {customer.FirstName}");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("customer has not been created");
+
+            //}
+
+            //List<Customer> customers = customerService.ReadCustomers();
+
+            //foreach(Customer customer in customers)
+            //    {
+            //    Console.WriteLine(customer);
+            //}
+
             Customer customer = new()
             {
-                FirstName = "Christos",
-                LastName = "Christou",
-                Address = "Athens"
+                Email = "ioannis@gmail",
+                Address = "Serres"
             };
-            Console.WriteLine(customer.ToString());
-            Console.WriteLine(customer.CustomerId);
-            Console.WriteLine(customer.FirstName);
-            Console.WriteLine(customer.LastName);
-            Console.WriteLine(customer.Email);
+
+            Customer dbCustomer = customerService.UpdateCustomer(3, customer);
+
+            Console.WriteLine(dbCustomer);
 
         }
     }
---
+++ CRM-App/Service/CustomerService.cs
@@ -0,0 +1,74 @@
+﻿using CRM_App.Database;
+using CRM_App.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_App.Service
+{
+    public class CustomerService
+    {
+        private CrmDbContext db;
+        
+        public CustomerService(CrmDbContext _db)
+        {
+            db = _db;
+        }
+        
+        public Customer CreateCustomer(Customer customer)
+        {
+
+
+            //validation of the customer first
+            if (customer == null)
+            {
+                return null;
+            }
+
+            if (customer.Email == null)
+            {
+                return null;
+            }
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return customer;
+
+        }
+
+
+        public List<Customer> ReadCustomers()
+        {
+            
+            return db.Customers.ToList();
+        }
+
+
+        public Customer UpdateCustomer(int customerId, Customer customer)
+        {
+             
+            Customer dbCustomer = db.Customers.Find(customerId);
+            if (dbCustomer == null) return null;
+            dbCustomer.Address = customer.Address;
+            dbCustomer.Email = customer.Email;
+            db.SaveChanges();
+
+            return dbCustomer;
+
+        }
+
+
+        public bool DeleteCustomer(int customerId)
+        {
+
+             
+            Customer dbCustomer = db.Customers.Find(customerId);
+            if (dbCustomer == null) return false;
+            db.Customers.Remove(dbCustomer);
+            return true;
+        }
+    }
+}
