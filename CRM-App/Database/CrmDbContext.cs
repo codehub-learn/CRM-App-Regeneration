7e2f6b7d4fe78db5c4ab3cd0307951f052b1a41e
dimitris <iracleous@gmail.com>
2021-05-12 19:49:18 +0300

    Migration

---
+++ CRM-App/Database/CrmDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CRM_App.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_App.Database
+{
+    public class CrmDbContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=RegenerationCrm;Integrated Security=True");
+        }
+    }
+   
+
+}
