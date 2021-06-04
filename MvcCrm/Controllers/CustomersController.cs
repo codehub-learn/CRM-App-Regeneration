1ea21deb8270f4759c4c698dbdad10b1b20814f6
dimitris <iracleous@gmail.com>
2021-06-04 19:51:22 +0300

    using ajax

--- MvcCrm/Controllers/CustomersController.cs
+++ MvcCrm/Controllers/CustomersController.cs
@@ -54,7 +54,7 @@ namespace MvcCrm.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CustomerId,FirstName,LastName,Address,Email,Username,Password,RegistrationDate")] Customer customer)
+        public async Task<IActionResult> Create(  [Bind("CustomerId,FirstName,LastName,Address,Email,Username,Password,RegistrationDate")] Customer customer)
         {
             if (ModelState.IsValid)
             {
--- MvcCrm/wwwroot/js/site.js
+++ MvcCrm/wwwroot/js/site.js
@@ -2,3 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('#CreateCustomer').on('click',
+
+    (event) => {
+        let FirstName = $('#FirstName').val();
+        let LastName = $('#LastName').val();
+        let Address = $('#Address').val();
+        let Email = $('#Email').val();
+        let Username = $('#Username').val();
+        let Password = $('#Password').val();
+        let RegistrationDate = $('#RegistrationDate').val();
+         
+        let data = JSON.stringify({
+            FirstName: FirstName,
+            LastName: LastName,
+            Address: Address,
+            Email: Email,
+            Username: Username,
+            Password: Password,
+            RegistrationDate: RegistrationDate 
+        });
+
+        console.log(data)
+
+        $.ajax({
+            url: `/api/Customers1`,
+            method: 'POST',
+            contentType: 'application/json',
+            data: data
+        }).done(response => {
+
+            console.log(response);
+
+            console.log('The registration was  successful');
+            $('#responseDiv').html('The registration was  successful');
+
+
+        }).fail(failure => {
+
+            $('#responseDiv').html('Error in the communication')
+        }).always(response => {
+            console.log(response);
+        });
+
+
+
+
+    }
+);
